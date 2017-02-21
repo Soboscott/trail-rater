a34bed522eed4d1efedf04a16af60685954b2236
Soboscott <scott.landry01772@yahoo.com>
2017-02-21 17:08:46 -0500

    added the create, destroy, index, update and destroy to reviews and trails

---
+++ scripts/trails/destroy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/trails"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request DELETE \
+  # --header "Authorization: Token token=$TOKEN"
+
+echo
