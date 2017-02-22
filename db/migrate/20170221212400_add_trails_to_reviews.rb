291ce679848b77747657af36887cb2f37ea69276
Soboscott <scott.landry01772@yahoo.com>
2017-02-22 13:19:39 -0500

    changed trails to trail

--- db/migrate/20170221212400_add_trails_to_reviews.rb
+++ db/migrate/20170221212400_add_trails_to_reviews.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class AddTrailsToReviews < ActiveRecord::Migration[5.0]
   def change
-    add_column :reviews, :trail, :refrences
+    add_reference :reviews, :trail, foreign_key: true, index: true
   end
 end
