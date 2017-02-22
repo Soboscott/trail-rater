1852c8a47c7bf068a3227df03f40298f3f106bbe
Soboscott <scott.landry01772@yahoo.com>
2017-02-22 13:14:34 -0500

    add events and styles for dropdown and comment fields

--- browser-template/assets/scripts/auth/events.js
+++ browser-template/assets/scripts/auth/events.js
@@ -1,67 +1,63 @@
 'use strict';
-//
-// const success = (data) => {
-//   if (data) { console.log(data); }
-// };
-//
-// const failure = (error) => {
-//   console.error(error);
-// };
-const signUpSuccess =() =>{
-  $('#sign-up1').dropdown('toggle');
-  $('#sign-up').val('');
 
+// TODO get cool switch thing to work
+
+const getFormFields = require(`../../../lib/get-form-fields`);
+
+const api = require('./api');
+const ui = require('./ui');
+const store = require('../store');
+
+const onSignUp = function (event) {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+    ;
 };
-const signInSuccess = () => {
-  // $('#sign-in1').dropdown('toggle');
 
-  // $('#sign-up').val('');
-  $('#sign-in');
-  $('#sign-up-btb').hide();
-  $('.signinhide').hide();
-  $('#getGame').show();
-  // $('#change-password').show();
-  $('#sign-out').show();
-  $('#reset').show();
-  $('h2').show();
-  $('.changebtn').show();
-  $('#wrapper').show();
+const onSignIn = function (event) {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  api.signIn(data)
+    .then((response) => {
+      store.user = response.user;
+      return store.user;
+    })
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+    ;
 };
-const signOutSuccess = () => {
-  $('#sign-out').hide();
-  $('#change-password').hide();
-  $('#getGame').hide();
-  $('#reset').hide();
-  $('#wrapper').hide();
-  $('.history').hide().val('');
-  $('#sign-in').show();
-  $('#sign-up').show();
-  $('.showTurn').hide();
-  $('.win').empty();
-  $('.history').empty();
-  $('.signinhide').show();
-  $('#sign-up-btb').show();
-  $('.changebtn').hide();
 
-
+const onChangePass = function (event) {
+  event.preventDefault();
+  let data = getFormFields(event.target);
+  api.changePass(data)
+    .then(ui.changePassSuccess)
+    .catch(ui.changePassFailure)
+    ;
 };
-const createGameSuccess = () => {
-  $('#wrapper').show();
-  $('.history').empty();
-  $('.win').show();
-};
-const getGameSuccess = (data) => {
-  // $('.history').show();
-  $('.history').text('You\'ve played ' + data.games.length + ' times!');
 
+const onSignOut = function (event) {
+  event.preventDefault();
+  api.signOut()
+    .then(() => {
+      delete store.user;
+      return store;
+    })
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+    ;
+};
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp);
+  $('#sign-in').on('submit', onSignIn);
+  $('#change-password').on('submit', onChangePass);
+  $('#sign-out').on('submit', onSignOut);
 };
 
 module.exports = {
-  signInSuccess,
-  signOutSuccess,
-  createGameSuccess,
-  getGameSuccess,
-  // success,
-  // failure,
-  signUpSuccess,
+  addHandlers,
 };
