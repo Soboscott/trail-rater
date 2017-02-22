6b436cb4d9e503915ec707de57b93f3469cc962f
Soboscott <scott.landry01772@yahoo.com>
2017-02-22 13:18:22 -0500

    changed column to reference

--- db/migrate/20170221212334_add_users_to_reviews.rb
+++ db/migrate/20170221212334_add_users_to_reviews.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class AddUsersToReviews < ActiveRecord::Migration[5.0]
   def change
-    add_column :reviews, :user, :refrences
+    add_reference :reviews, :user, foreign_key: true, index: true
   end
 end
